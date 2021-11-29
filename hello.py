f015558e50b2ac6fcddc320ca84866ff7feb42f6
bebs99 <42590965+bebs99@users.noreply.github.com>
2021-11-29 20:34:36 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from app import app
+from flask import render_template
+
 app = Flask(__name__)
  
-@app.route('/')
-def hello_world():
-    return 'home.html'
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return """
+    <h1 style='color: red;'>I'm a red H1 heading!</h1>
+    <p>This is a lovely little paragraph</p>
+    <code>Flask is <em>awesome</em></code>
+    """
