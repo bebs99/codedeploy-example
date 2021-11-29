71f70a2c09139cafebc108a07d3061bd61d787b8
bebs99 <42590965+bebs99@users.noreply.github.com>
2021-11-29 03:32:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import Flask, render_template
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template('home.html')
+
+if __name__ == "__main__":
+ app.run()
