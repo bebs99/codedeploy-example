50c63d75e76eb5ac53bcf9edbe192c4a17c34dce
bebs99 <42590965+bebs99@users.noreply.github.com>
2021-11-29 02:39:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from markupsafe import escape
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route("/")
+def hello(home):
+    return f"Hello, {escape(home)}!"
