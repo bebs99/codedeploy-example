3758cc5ac79495704935ab1ae4513f899b497bf0
bebs99 <42590965+bebs99@users.noreply.github.com>
2021-11-29 04:41:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
+from flask import Flask, render_template, redirect, url_for, request# Route for handling the login page logic
+app = Flask(__name__)@app.route('/', methods=['GET', 'POST'])
 def home():
-  return render_template('home.html')
-
-if __name__ == "__main__":
- app.run()
+    return render_template('home.html')#within app.route add the html page we are doing changes to
+@app.route('/')
+# def is normally how we define a function in python
+def home():
+ return render_template('home.html')
