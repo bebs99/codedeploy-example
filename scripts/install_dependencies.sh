7876cee3f8cbbc32b9331ccb08ae410525066b3b
bebs99 <42590965+bebs99@users.noreply.github.com>
2021-11-30 18:18:58 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python2.7 python-pip
+#sudo apt-get install -y python2.7 python-pip
 sudo curl https://bootstrap.pypa.io/get-pip.py | python3.6 - --user
 pip3 install -r requirements.txt
-
-#pip install Flask
+pip install Flask
+pip install gunicorn
+pip install jinja2
