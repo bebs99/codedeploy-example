eb0dbe700b362df46e08a6a89aa568b621587aeb
bebs99 <42590965+bebs99@users.noreply.github.com>
2021-11-30 18:08:30 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get install -y python2.7 python-pip
 sudo apt-get install -y python3.6 python-pip
+sudo apt-get install python3-pip
 pip3 install -r requirements.txt
 
 #pip install Flask
